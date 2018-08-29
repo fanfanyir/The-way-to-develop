af8267a6897e1ea35580c50034ac2fb325a3f9e6
yifan.zhang03 <yifan.zhang03@ele.me>
2018-08-29 10:20:34 +0800

    learn vue

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+E
+vue-src/core/index.js,4/a/4a1063e59087d78ec05256576f256e8ad8b4b819
--- vue-src/core/index.js
+++ vue-src/core/index.js
@@ -1,9 +1,13 @@
 import Vue from './instance/index'
+//初始化全局API
 import { initGlobalAPI } from './global-api/index'
+//
 import { isServerRendering } from 'core/util/env'
 
+//执行初始化全局变量
 initGlobalAPI(Vue)
 
+//为vue原型定义$isServer
 Object.defineProperty(Vue.prototype, '$isServer', {
   get: isServerRendering
 })
