c109b090150584881b0c91731f12dde00e5d2dcc
fanfanyir <fanfanyir@163.com>
2018-12-21 23:56:08 +0800

    react-redux 删除

--- react/react-redux/src/TodoList.js
+++ react/react-redux/src/TodoList.js
@@ -7,30 +7,20 @@ class TodoList extends  Component {
   constructor(props){
     super(props);
     this.state = store.getState();
-    this.handleClick = this.handleClick.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   render (){
     return (
       <div>
         <div>
-          <input value={this.props.inputValue} onChange={this.handleInputChange}/>
-          <button onClick={this.handleClick}>提交</button>
+          <input value={this.props.inputValue} onChange={this.props.changeInputValue}/>
+          <button onClick={this.props.handleClick}>提交</button>
         </div>
         <ul>
           <li>dell</li>
         </ul>
       </div>
     )
-  }
-
-  handleInputChange(e){
-    console.log(e.target.value)
-  }
-
-  handleClick(){
-
   }
 }
 
@@ -41,4 +31,23 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps,null)(TodoList);
+// store.dispatch, props
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeInputValue(e){
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action)
+    },
+    handleClick(){
+      const action = {
+        type: 'add_item_list'
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoList);
--- react/react-redux/src/store/reducer.js
+++ react/react-redux/src/store/reducer.js
@@ -5,5 +5,15 @@ const defaultState = {
 
 
 export default (state = defaultState , action) => {
+  if(action.type === 'change_input_value'){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value;
+    return newState;
+  }
+  if(action.type === 'add_item_list'){
+    const newState =  JSON.parse(JSON.stringify(state));
+    newState.list.push(state.inputValue);
+    return newState;
+  }
   return state;
 }
