5c7f032ba718f19f36ce6b88530d89233ae7efdb
fanfanyir <fanfanyir@163.com>
2018-12-23 00:56:27 +0800

    获取 List

--- react/janshu/src/common/header/store/actionCreators.js
+++ react/janshu/src/common/header/store/actionCreators.js
@@ -1,4 +1,6 @@
 import * as constants from './constants';
+import axios from 'axios';
+import { fromJS } from 'immutable';
 
 export const getFocused = (focused) => ({
   type: constants.SEARCH_FOCUSED,
@@ -10,3 +12,18 @@ export const getBlur = (focused) => ({
   type: constants.SEARCH_BLUR,
   focused
 })
+
+const changeList = (data) => ({
+  type: constants.CHANGE_LIST,
+  data: fromJS(data)
+})
+export const getList = () => {
+  return (dispatch) => {
+    axios.get('/api/headerList.json').then((res) => {
+      const data = res.data;
+      dispatch(changeList(data.data))
+    }).catch(() => {
+      console.log('error')
+    })
+  }
+}
