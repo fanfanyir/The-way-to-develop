0417d554bdccc3b37ab5a9d264bda39fe21e9f0e
fanfanyir <fanfanyir@163.com>
2019-01-03 23:01:46 +0800

    面向对象

---
+++ 神奇的js/understand-js/mixin(extend).js
@@ -0,0 +1,35 @@
+// 简单的 继承/混入 的例子
+function mixin( sourceObj, targetObj ){
+  for(var key in sourceObj){
+    // 只会在不存在的情况下复制
+    if(!(key in targetObj)){
+      targetObj[key] = sourceObj[key];
+    }
+  }
+
+  return targetObj;
+}
+
+var Vehicle = {
+  engines: 1,
+
+  ignition: function() {
+    console.log("Turning on my engine.");
+  },
+
+  drive: function() {
+    this.ignition();
+    console.log("Steering and moving forward");
+  }
+};
+
+var Car = mixin (Vehicle, {
+  wheels: 4,
+
+  drive: function(){
+    Vehicle.drive.call(this);
+    console.log(
+      "Rolling on all " + this.wheels + " wheels !"
+    );
+  }
+});
